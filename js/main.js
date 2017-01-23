466a99909c46cdfd95353a7846bd0181ec8f9824
Mohsin Rahman <mohsinrahmang@gmail.com>
2017-01-23 23:29:29 +0100

    kontakt form

--- js/main.js
+++ js/main.js
@@ -4,3 +4,20 @@ jQuery(function($){
         $('.navbar .navbar-collapse').collapse('hide')
     });
 });
+
+
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(59.334591, 18.063240),
+    zoom: 7,
+    zoomControl: true,
+    mapTypeControlOptions: {
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+      position: google.maps.ControlPosition.TOP_CENTER,
+      animation: google.maps.Animation.BOUNCE
+    }
+  };
+
+  var map = new google.maps.Map(mapCanvas ,mapOptions);
+}
